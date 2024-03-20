be99f9b478d24957cb4fa5a688bd966989f1710e
sinamong0620 <sinaskim60@gmail.com>
2024-03-20 10:47:57 +0900

    Feat : 로그인 버튼 추가
    로그인 컴포넌트 버튼 디자인 완료

---
+++ chatskhu-fe/pages/login/LoginButton.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+const LoginButton = () => {
+  return (
+    <KaKaoButtonStyled>
+      <Image src="/images/chat.png" alt="chat_image" width={24} height={24} />
+      카카오로 3초 만에 시작하기
+    </KaKaoButtonStyled>
+  );
+};
+export default LoginButton;
+
+const KaKaoButtonStyled = styled.button`
+  width: 242px;
+  height: 45px;
+
+  background-color: #fee500;
+  border: none;
+  border-radius: 10px;
+
+  display: flex;
+  flex-direction: center;
+  align-items: center;
+  font-weight: 500;
+
+  img {
+    margin-right: 14px;
+  }
+`;
