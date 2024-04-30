2da3a6954b5f0c8450ff98f8b743b6d2e1e9e789
sinamong0620 <sinaskim60@gmail.com>
2024-04-30 23:57:50 +0900

    #9 feat: 대화 컴포넌트 추가

--- chatskhu-fe/components/Conversation.tsx
+++ chatskhu-fe/components/Conversation.tsx
@@ -1,7 +1,38 @@
+import styled from 'styled-components';
+import Profile from '../public/images/kakaoprofileimage.png';
+import Image from 'next/image';
+
 const Conversation = () => {
-   return (
-      <>
-         <div>Next.j 프로젝트 진행 중</div>
-      </>
-   )
-}
+    return (
+        <ConversationContainer>
+            <UserInfoContainer>
+                <Image src={Profile} alt={'프로필 이미지'} height={40} width={40} />
+                <span>YOU</span>
+            </UserInfoContainer>
+            <p>수강신청 하는 방법 알려줘</p>
+        </ConversationContainer>
+    );
+};
+export default Conversation;
+
+const ConversationContainer = styled.div`
+    span {
+        font-size: 1rem;
+        font-weight: bold;
+    }
+
+    p {
+        font-size: 0.8rem;
+        font-weight: 200;
+    }
+`;
+
+const UserInfoContainer = styled.div`
+    margin-bottom: 0.4rem;
+
+    img {
+        border-radius: 100%;
+        margin-right: 0.4rem;
+        margin-bottom: 0rem;
+    }
+`;
