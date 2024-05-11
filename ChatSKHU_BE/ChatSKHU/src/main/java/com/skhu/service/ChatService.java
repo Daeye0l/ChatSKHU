11ae00e7f0ee6bc59658af5d4e63c1c38882c8bc
Kim, Seokwan <sukwan1106@gmail.com>
2024-05-12 02:14:45 +0900

    ChatRoom 생성 #49

--- ChatSKHU_BE/ChatSKHU/src/main/java/com/skhu/service/ChatService.java
+++ ChatSKHU_BE/ChatSKHU/src/main/java/com/skhu/service/ChatService.java
@@ -1,12 +1,13 @@
 package com.skhu.service;
 
-import com.skhu.dto.FlaskRequest;
-import com.skhu.dto.FlaskResponse;
-import com.skhu.dto.GPTRequest;
-import com.skhu.dto.GPTResponse;
+import com.skhu.domain.ChatRoom;
+import com.skhu.domain.User;
+import com.skhu.dto.*;
 import com.skhu.domain.Chat;
 import com.skhu.repository.ChatRepository;
 
+import com.skhu.repository.ChatRoomRepository;
+import com.skhu.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
@@ -17,50 +18,73 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 @Service
 @RequiredArgsConstructor
 public class ChatService {
-	private final ChatRepository chatRepository;
-	private final RestTemplate restTemplate;
+    private final ChatRepository chatRepository;
+    private final RestTemplate restTemplate;
+    private final ChatRoomRepository chatRoomRepository;
+    private final UserRepository userRepository;
 
-	@Value("${openai.model}")
-	private String model;
-	
-	@Value("${openai.gpt-api-key}")
-	private String apiKey;
-	
-	@Transactional
-	public String chat(String question) {
-		FlaskResponse flaskResponse = getPrompt(question);
-		GPTRequest gptRequest = new GPTRequest(model, flaskResponse.getPrompt(), 256);
-		
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("Authorization", "Bearer " + apiKey);
-		HttpEntity<GPTRequest> entity = new HttpEntity<>(gptRequest, headers);
-		
-		GPTResponse gptResponse = restTemplate.postForObject("https://api.openai.com/v1/chat/completions", entity, GPTResponse.class);
-		String answer = gptResponse.getChoices().get(0).getMessage().getContent();
-		
-		save(question, answer);
-		
-		return answer;
-	}
-	
-	@Transactional
-	public void save(String question, String answer) {
-		chatRepository.save(Chat.builder()
-				.question(question)
-				.answer(answer)
-				.build());
-	}
-	
-	@Transactional
-	public FlaskResponse getPrompt(String question) {
-		FlaskRequest flaskRequest = new FlaskRequest(question);
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		HttpEntity<FlaskRequest> entity = new HttpEntity<>(flaskRequest, headers);
-		FlaskResponse flaskResponse = restTemplate.postForObject("http://localhost:8085/prompt", entity, FlaskResponse.class);
-		return flaskResponse;
-	}
+    @Value("${openai.model}")
+    private String model;
+
+    @Value("${openai.gpt-api-key}")
+    private String apiKey;
+
+    @Transactional
+    public String chat(String question) {
+        FlaskResponse flaskResponse = getPrompt(question);
+        GPTRequest gptRequest = new GPTRequest(model, flaskResponse.getPrompt(), 256);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + apiKey);
+        HttpEntity<GPTRequest> entity = new HttpEntity<>(gptRequest, headers);
+
+        GPTResponse gptResponse = restTemplate.postForObject("https://api.openai.com/v1/chat/completions", entity, GPTResponse.class);
+        String answer = gptResponse.getChoices().get(0).getMessage().getContent();
+        save(question, answer);
+
+        return answer;
+    }
+
+    @Transactional
+    public void save(String question, String answer) {
+        chatRepository.save(Chat.builder()
+                .question(question)
+                .answer(answer)
+                .build());
+    }
+
+    @Transactional
+    public FlaskResponse getPrompt(String question) {
+        FlaskRequest flaskRequest = new FlaskRequest(question);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<FlaskRequest> entity = new HttpEntity<>(flaskRequest, headers);
+        FlaskResponse flaskResponse = restTemplate.postForObject("http://localhost:8085/prompt", entity, FlaskResponse.class);
+        return flaskResponse;
+    }
+
+    @Transactional
+    public ChatRoomResponseDto createChatRoom(String title, String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow();
+
+        ChatRoom chatRoom = chatRoomRepository.save(ChatRoom.builder()
+                .title(title)
+                .user(user)
+                .build());
+        return ChatRoomResponseDto.builder()
+                .title(chatRoom.getTitle())
+                .userId(chatRoom.getUser().getId())
+                .id(chatRoom.getId())
+                .build();
+    }
+
+
 }
--- ChatSKHU_BE/ChatSKHU/src/main/java/com/skhu/service/EncryptionService.java
+++ ChatSKHU_BE/ChatSKHU/src/main/java/com/skhu/service/EncryptionService.java
@@ -1,6 +1,5 @@
 package com.skhu.service;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
