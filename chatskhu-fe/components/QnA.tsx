3969f11b697e576641b6d781847b45e542e7f625
sinamong0620 <sinaskim60@gmail.com>
2024-05-13 17:33:19 +0900

    #17 design : 사이드바 대화 누르면 사이드바 다시 들어가도록 수정

--- chatskhu-fe/components/QnA.tsx
+++ chatskhu-fe/components/QnA.tsx
@@ -1,44 +1,44 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { useStore } from '../store/\bstore';
 
 interface Props {
-   month: string
+    month: string;
 }
 const QnA = ({ month }: Props) => {
-   return (
-      <QnAContainer>
-         <DateStyle>{month}</DateStyle>
-         {/* 이 날 값이 존재하면 ol 배열 출력 */}
-         {
-            <ConversationList>
-               <li>
-                  쿡쿡fdsafsjalkfjsalkfdjsklafjslkajflkfdsafdsafkldsajfksjflksjaflkjasljfljsafjkjalfja
-               </li>
-            </ConversationList>
-         }
-      </QnAContainer>
-   )
-}
-export default QnA
+    const { setIsOpen } = useStore();
+    return (
+        <QnAContainer>
+            <DateStyle>{month}</DateStyle>
+            {
+                <ConversationList onClick={(prev) => setIsOpen(!prev)}>
+                    <li>쿡쿡fdsafsjalkfjsalkfdjsklafjslkajflkfdsafdsafkldsajfksjflksjaflkjasljfljsafjkjalfja</li>
+                </ConversationList>
+            }
+        </QnAContainer>
+    );
+};
+export default QnA;
 
 const QnAContainer = styled.div`
-   margin-top: 1.25rem;
-`
+    margin-top: 1.25rem;
+`;
 const DateStyle = styled.div`
-   padding: 0.75rem 0.5rem 0.5rem 0.5rem;
-   color: gray;
-   font-size: 0.7rem;
-   font-weight: bold;
-`
+    padding: 0.75rem 0.5rem 0.5rem 0.5rem;
+    color: gray;
+    font-size: 0.7rem;
+    font-weight: bold;
+`;
 const ConversationList = styled.ol`
-   li {
-      padding: 0.5rem;
-      border-radius: 0.5rem;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
+    li {
+        padding: 0.5rem;
+        border-radius: 0.5rem;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
 
-      &:hover {
-         background-color: #ececec;
-      }
-   }
-`
+        &:hover {
+            background-color: #ececec;
+            cursor: pointer;
+        }
+    }
+`;
--- chatskhu-fe/components/Sidebar.tsx
+++ chatskhu-fe/components/Sidebar.tsx
@@ -1,179 +1,141 @@
-import React from 'react'
-import styled from 'styled-components'
-import Header from './Header'
-import Image from 'next/image'
-import closebutton from '/public/images/closebutton.png'
-import kakaoProfileImage from '/public/images/kakaoprofileimage.png'
-import { theme } from '../styles/theme'
-import { useStore } from '../store/\bstore'
-import { motion, AnimatePresence } from 'framer-motion'
-import QnA from './QnA'
+import React from 'react';
+import styled from 'styled-components';
+import Header from './Header';
+import Image from 'next/image';
+import closebutton from '/public/images/closebutton.png';
+import kakaoProfileImage from '/public/images/kakaoprofileimage.png';
+import { theme } from '../styles/theme';
+import { useStore } from '../store/\bstore';
+import { motion, AnimatePresence } from 'framer-motion';
+import QnA from './QnA';
 
 const SidebarComponent = () => {
-   const { setIsOpen } = useStore()
-   // //날짜 생성 변수 (나중엔 백엔드에서 받아올 것)
-   // const date = new Date()
+    const { setIsOpen } = useStore();
 
-   // const timeformat = new Intl.DateTimeFormat('en-US', {
-   //    month: 'long',
-   // }).format(date)
-
-   return (
-      <Container>
-         <SideBarContainer>
-            <SideBar>
-               <div>
-                  <Header
-                     width='100%'
-                     border='none'
-                     padding='0.8rem 0em'
-                     src='hunsu'
-                     position='sticky'
-                  />
-                  <nav>
-                     <QnA month={'Today'} />
-                     <QnA month={'YesterDay'} />
-                     <QnA month={'YesterDay'} />
-                     <QnA month={'YesterDay'} />
-                     <QnA month={'YesterDay'} />
-                     <QnA month={'YesterDay'} />
-                     <QnA month={'YesterDay'} />
-                     <QnA month={'YesterDay'} />
-                     <QnA month={'YesterDay'} />
-                     <QnA month={'YesterDay'} />
-                     <QnA month={'YesterDay'} />
-                     <QnA month={'YesterDay'} />
-                     <QnA month={'YesterDay'} />
-                     <QnA month={'YesterDay'} />
-                  </nav>
-               </div>
-               <SideBarFooter>
-                  <div>
-                     <Image
-                        src={kakaoProfileImage}
-                        width={40}
-                        height={40}
-                        alt='kakaoImageProfile'
-                     />
-                     <p>sina</p>
-                  </div>
-               </SideBarFooter>
-            </SideBar>
-            <button onClick={(prev) => setIsOpen(!prev)}>
-               <Image
-                  src={closebutton}
-                  alt='hunsu_img'
-                  width={15}
-                  height={15}
-               />
-            </button>
-         </SideBarContainer>
-         <AnimatePresence>
-            <SideBarBackground
-               key='modal'
-               initial={{ opacity: 0 }}
-               animate={{
-                  opacity: 1,
-               }}
-               exit={{ opacity: 0 }}
-            ></SideBarBackground>
-            {/* //일어나면 여기 다시 고쳐보기 */}
-         </AnimatePresence>
-      </Container>
-   )
-}
-export default SidebarComponent
+    return (
+        <Container>
+            <SideBarContainer>
+                <SideBar>
+                    <div>
+                        <Header width="100%" border="none" padding="0.8rem 0em" src="hunsu" position="sticky" />
+                        <nav>
+                            <QnA month={'Today'} />
+                            <QnA month={'Previous 7 Days'} />
+                            <QnA month={'Previous 30 Days'} />
+                            <QnA month={'April'} />
+                            <QnA month={'March'} />
+                        </nav>
+                    </div>
+                    <SideBarFooter>
+                        <div>
+                            <Image src={kakaoProfileImage} width={40} height={40} alt="kakaoImageProfile" />
+                            <p>sina</p>
+                        </div>
+                    </SideBarFooter>
+                </SideBar>
+                <button onClick={(prev) => setIsOpen(!prev)}>
+                    <Image src={closebutton} alt="hunsu_img" width={15} height={15} />
+                </button>
+            </SideBarContainer>
+            <AnimatePresence>
+                <SideBarBackground
+                    key="modal"
+                    initial={{ opacity: 0 }}
+                    animate={{
+                        opacity: 1,
+                    }}
+                    exit={{ opacity: 0 }}
+                ></SideBarBackground>
+            </AnimatePresence>
+        </Container>
+    );
+};
+export default SidebarComponent;
 
 const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  position: absolute;
-  top: 0;
-  left: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    position: absolute;
+    top: 0;
+    left: 0;
 
-  button {
-    height: fit-content;
-    border: 0;
-    cursor: pointer;
+    button {
+        height: fit-content;
+        border: 0;
+        cursor: pointer;
 
-    margin-top: 3em;
-    margin-left: 0.5em;
+        margin-top: 3em;
+        margin-left: 0.5em;
 
-    color: white;
-    background-color: transparent;
-    font-size: ${theme.fontSize.extraLarge};
-  }
-`
+        color: white;
+        background-color: transparent;
+        font-size: ${theme.fontSize.extraLarge};
+    }
+`;
 const SideBarContainer = styled(motion.div)`
-  width: auto;
-  position: absolute;
-  height: 100vh;
-  z-index: 100;
-  display: flex;
+    width: auto;
+    position: absolute;
+    height: 100vh;
+    z-index: 100;
+    display: flex;
 
-  ::-webkit-scrollbar {
-    width: 0.5rem;
+    ::-webkit-scrollbar {
+        width: 0.5rem;
+    }
 
-  }
-
-  ::-webkit-scrollbar-thumb {
-    background-color: #e8e8e8;
-    border-radius: 10rem;
-  }
-
-`
+    ::-webkit-scrollbar-thumb {
+        background-color: #e8e8e8;
+        border-radius: 10rem;
+    }
+`;
 const SideBar = styled.nav`
-  width: 20rem;
-  display: flex;
-  flex-direction: column;
-  background-color: #f9f9f9;
+    width: 20rem;
+    display: flex;
+    flex-direction: column;
+    background-color: #f9f9f9;
 
+    &::-webkit-scrollbar {
+        width: 0.4rem;
+    }
 
-  &::-webkit-scrollbar {
-    width: 0.4rem;
-  }
+    &::-webkit-scrollbar-thumb {
+        background-color: black;
+        border-radius: 10rem;
+    }
 
-  &::-webkit-scrollbar-thumb {
-    background-color: black;
-    border-radius: 10rem;
-  }
-
-
-  & > div {
-    overflow-y: auto;
-    background-color: #f8f8f8;
-    padding: 0 0.8rem;
-  }
-
-`
+    & > div {
+        overflow-y: auto;
+        background-color: #f8f8f8;
+        padding: 0 0.8rem;
+    }
+`;
 
 const SideBarBackground = styled(motion.div)`
-  width: 100%;
-  background: rgba(0, 0, 0, 0.3);
-  z-index: 1;
-`
+    width: 100%;
+    background: rgba(0, 0, 0, 0.3);
+    z-index: 1;
+`;
 
 const SideBarFooter = styled.footer`
-  & > div {
-    padding : .5rem;
-    display: flex;
-    align-items: center;
-    gap: 0.875rem;
-    border-radius: ${theme.InputRadius.radius};
-  }
-  
-  & > div:hover {
-    background-color: #ececec;
-  }
-  div img {
-    border-radius: 100%;
-  }
-  position: sticky;
-  bottom: 0;
-  padding: 0.8rem;
-  background-color: #f8f8f8;
+    & > div {
+        padding: 0.5rem;
+        display: flex;
+        align-items: center;
+        gap: 0.875rem;
+        border-radius: ${theme.InputRadius.radius};
+    }
 
-`
-// 수정사항 : 헤더랑 푸터 둘다 배경이 투명해서 지나가는거 다보임 ㅋㅋㅋ
-// 그리고 스크롤 호버할때만 보이도록 수정
+    & > div:hover {
+        background-color: #ececec;
+        cursor: pointer;
+    }
+    div img {
+        border-radius: 100%;
+    }
+    position: sticky;
+    bottom: 0;
+    top: 10000px;
+    padding: 0.8rem;
+    background-color: #f8f8f8;
+`;
