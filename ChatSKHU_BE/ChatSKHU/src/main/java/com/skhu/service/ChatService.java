a3bd3e4b4f97dc38f196a543125dff5ad2b5cbab
Daeyeol Sung <tjdeoduf1228@gmail.com>
2024-05-13 10:37:53 +0900

    충돌해결

--- ChatSKHU_BE/ChatSKHU/src/main/java/com/skhu/service/ChatService.java
+++ ChatSKHU_BE/ChatSKHU/src/main/java/com/skhu/service/ChatService.java
@@ -1,19 +1,25 @@
 package com.skhu.service;
 
+import com.skhu.dto.ChatRoomResponseDto;
+import com.skhu.dto.ChatRoomsResponseDto;
+import com.skhu.dto.FlaskRequest;
+import com.skhu.dto.FlaskResponse;
+import com.skhu.dto.GPTRequest;
+import com.skhu.dto.GPTResponse;
+import com.skhu.domain.Chat;
 import com.skhu.domain.ChatRoom;
 import com.skhu.domain.User;
-import com.skhu.dto.*;
-import com.skhu.domain.Chat;
-import com.skhu.domain.User;
 import com.skhu.repository.ChatRepository;
-import com.skhu.repository.UserRepository;
-
 import com.skhu.repository.ChatRoomRepository;
 import com.skhu.repository.UserRepository;
+
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -22,60 +28,58 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
-
-
 @Service
 @RequiredArgsConstructor
 public class ChatService {
-<<<<<<< HEAD
-    private final ChatRepository chatRepository;
-    private final RestTemplate restTemplate;
-    private final ChatRoomRepository chatRoomRepository;
-    private final UserRepository userRepository;
+	private final ChatRepository chatRepository;
+	private final UserRepository userRepository;
+	private final RestTemplate restTemplate;
+	private final ChatRoomRepository chatRoomRepository;
 
-    @Value("${openai.model}")
-    private String model;
-
-    @Value("${openai.gpt-api-key}")
-    private String apiKey;
-
-    @Transactional
-    public String chat(String question) {
-        FlaskResponse flaskResponse = getPrompt(question);
-        GPTRequest gptRequest = new GPTRequest(model, flaskResponse.getPrompt(), 256);
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + apiKey);
-        HttpEntity<GPTRequest> entity = new HttpEntity<>(gptRequest, headers);
-
-        GPTResponse gptResponse = restTemplate.postForObject("https://api.openai.com/v1/chat/completions", entity, GPTResponse.class);
-        String answer = gptResponse.getChoices().get(0).getMessage().getContent();
-        save(question, answer);
-
-        return answer;
-    }
-
-    @Transactional
-    public void save(String question, String answer) {
-        chatRepository.save(Chat.builder()
-                .question(question)
-                .answer(answer)
-                .build());
-    }
-
-    @Transactional
-    public FlaskResponse getPrompt(String question) {
-        FlaskRequest flaskRequest = new FlaskRequest(question);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<FlaskRequest> entity = new HttpEntity<>(flaskRequest, headers);
-        FlaskResponse flaskResponse = restTemplate.postForObject("http://localhost:8085/prompt", entity, FlaskResponse.class);
-        return flaskResponse;
-    }
-
-    @Transactional
+	@Value("${openai.model}")
+	private String model;
+	
+	@Value("${openai.gpt-api-key}")
+	private String apiKey;
+	
+	@Transactional
+	public String chat(String question, Principal principal) {
+		User user = userRepository.findByEmail(principal.getName()).orElseThrow();
+		FlaskResponse flaskResponse = getPrompt(question);
+		GPTRequest gptRequest = new GPTRequest(model, flaskResponse.getPrompt(), 256);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Bearer " + apiKey);
+		HttpEntity<GPTRequest> entity = new HttpEntity<>(gptRequest, headers);
+		
+		GPTResponse gptResponse = restTemplate.postForObject("https://api.openai.com/v1/chat/completions", entity, GPTResponse.class);
+		String answer = gptResponse.getChoices().get(0).getMessage().getContent();
+		
+		save(question, answer, user);
+		
+		return answer;
+	}
+	
+	@Transactional
+	public void save(String question, String answer, User user) {
+		chatRepository.save(Chat.builder()
+				.question(question)
+				.answer(answer)
+				.user(user)
+				.build());
+	}
+	
+	@Transactional
+	public FlaskResponse getPrompt(String question) {
+		FlaskRequest flaskRequest = new FlaskRequest(question);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<FlaskRequest> entity = new HttpEntity<>(flaskRequest, headers);
+		FlaskResponse flaskResponse = restTemplate.postForObject("http://localhost:8085/prompt", entity, FlaskResponse.class);
+		return flaskResponse;
+	}
+	
+	@Transactional
     public ChatRoomResponseDto createChatRoom(String title, String email) {
         User user = userRepository.findByEmail(email)
                 .orElseThrow();
@@ -135,53 +139,4 @@ public class ChatService {
                 .id(chatRoom.getId())
                 .build(); 
     }
-
-=======
-	private final ChatRepository chatRepository;
-	private final UserRepository userRepository;
-	private final RestTemplate restTemplate;
-
-	@Value("${openai.model}")
-	private String model;
-	
-	@Value("${openai.gpt-api-key}")
-	private String apiKey;
-	
-	@Transactional
-	public String chat(String question, Principal principal) {
-		User user = userRepository.findByEmail(principal.getName()).orElseThrow();
-		FlaskResponse flaskResponse = getPrompt(question);
-		GPTRequest gptRequest = new GPTRequest(model, flaskResponse.getPrompt(), 256);
-		
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("Authorization", "Bearer " + apiKey);
-		HttpEntity<GPTRequest> entity = new HttpEntity<>(gptRequest, headers);
-		
-		GPTResponse gptResponse = restTemplate.postForObject("https://api.openai.com/v1/chat/completions", entity, GPTResponse.class);
-		String answer = gptResponse.getChoices().get(0).getMessage().getContent();
-		
-		save(question, answer, user);
-		
-		return answer;
-	}
-	
-	@Transactional
-	public void save(String question, String answer, User user) {
-		chatRepository.save(Chat.builder()
-				.question(question)
-				.answer(answer)
-				.user(user)
-				.build());
-	}
-	
-	@Transactional
-	public FlaskResponse getPrompt(String question) {
-		FlaskRequest flaskRequest = new FlaskRequest(question);
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		HttpEntity<FlaskRequest> entity = new HttpEntity<>(flaskRequest, headers);
-		FlaskResponse flaskResponse = restTemplate.postForObject("http://localhost:8085/prompt", entity, FlaskResponse.class);
-		return flaskResponse;
-	}
->>>>>>> 6d6bd3a (chat user domain 연결)
 }
