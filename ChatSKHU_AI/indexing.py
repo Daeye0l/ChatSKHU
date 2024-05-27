18fe401d509103fb1645771f943fc232fc4ed1f8
Daeyeol Sung <tjdeoduf1228@gmail.com>
2024-05-27 20:57:02 +0900

    텍스트 데이터 사용, 청크 단위가 아닌 빈 줄 단위로 임베딩 #38

--- ChatSKHU_AI/indexing.py
+++ ChatSKHU_AI/indexing.py
@@ -1,9 +1,11 @@
 from langchain_community .vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores.utils import DistanceStrategy
 from splitText import splitText
 
-filePath = './retrievalData.pdf'
+filePath = './retrievalData.txt'
 splits = splitText(filePath)
-# 스플릿한 데이터를 임베딩해서 벡터 db에 저장
-vectorstore = FAISS.from_documents(documents = splits, embedding = OpenAIEmbeddings()) 
+# 스플릿한 데이터를 임베딩해서 벡터 db에 저장, 유사도 측정 기준을 유클리드 거리 기반에서 코사인 거리 기반으로 변경
+vectorstore = FAISS.from_texts(texts = splits, embedding = OpenAIEmbeddings(), distance_strategy = DistanceStrategy.COSINE)
+print(vectorstore.distance_strategy)
 vectorstore.save_local('./vectorStore') # 로컬에 벡터 db저장
--- ChatSKHU_AI/splitText.py
+++ ChatSKHU_AI/splitText.py
@@ -1,13 +1,11 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from loadData import loadData
 
 def splitText(filePath):
     docs = loadData(filePath)
-    # 분할기 지정, 500개의 청크로 조각내고 문장의 연결성을 유지하기 위해 100청크씩 오버랩
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 100)
-    splits = text_splitter.split_documents(docs)
+    splits = docs[0].page_content.split('\n\n') # 빈 줄 기준으로 텍스트 스플릿
 
     print(len(splits)) # 분할한 컨텐츠의 수
-    print(splits[0].page_content)
+    print(splits[0]) # 첫 번째 컨텐츠
+    print()
 
     return splits
