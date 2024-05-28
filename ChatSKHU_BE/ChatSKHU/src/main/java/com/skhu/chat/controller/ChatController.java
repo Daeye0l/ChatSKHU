aeff3f0893fd341ea63c30eef80dfb1d4b6186c8
Kim, Seokwan <sukwan1106@gmail.com>
2024-05-28 19:49:26 +0900

    첫 chat시 chatRoom 자동 생성

--- ChatSKHU_BE/ChatSKHU/src/main/java/com/skhu/chat/controller/ChatController.java
+++ ChatSKHU_BE/ChatSKHU/src/main/java/com/skhu/chat/controller/ChatController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import reactor.core.publisher.Flux;
 
 
 @RestController
@@ -24,43 +23,44 @@ import reactor.core.publisher.Flux;
 @RequiredArgsConstructor
 @Tag(name = "GPTController", description = "GPT 답변 생성 관련 API")
 public class ChatController {
-	private final ChatService chatService;
-	
-	@Operation(summary = "OpenAI API 호출", description = "GPT 답변 확인")
-	@PostMapping("/chat")
-	public ResponseEntity<Flux<Object>> chat(@RequestBody ChatDto.ChatRequest chatRequest, Principal principal) {
-		return ResponseEntity.ok(chatService.chat(chatRequest, principal.getName()));
-	}
-	
-	@Operation(summary = "ChatList 확인", description = "UserId에 따른 CreatedDate 최신순 Chat 목록 조회")
-	@GetMapping("/chat")
-	public ResponseEntity<List<ChatDto.ChatSearchResponse>> chatList(Principal principal) {
-		return ResponseEntity.ok(chatService.findByUserIdOrderByCreatedDateDesc(principal.getName()));
-	}
-	
-	@Operation(
-			summary = "ChatRoom 생성",
-			description = "ChatRoom 생성하는 API")
-	@PostMapping("/chatroom")
-	public ResponseEntity<ChatDto.ChatRoomResponse> chatRoom(@RequestBody ChatDto.ChatRoomRequest request, Principal principal){
-		return ResponseEntity.ok(chatService.createChatRoom(request, principal.getName()));
-	}
+    private final ChatService chatService;
 
-	@Operation(
-			summary = "날짜별 ChatRoom 조회",
-			description = "오늘, 어제, 근 일주일, 근 한달 그룹으로 ChatRoom 조회하는 API")
-	@GetMapping("/chatroom")
-	public ResponseEntity<ChatDto.ChatRoomsResponse> getChatRooms(Principal principal){
-		return ResponseEntity.ok(chatService.getChatRooms(principal.getName()));
-	}
+    @Operation(summary = "OpenAI API 호출", description = "GPT 답변 확인")
+    @PostMapping("/chat/{chatRoomId}")
+    public ResponseEntity<ChatDto.ChatResponse> chat(@PathVariable Long chatRoomId, @RequestBody ChatDto.ChatRequest chatRequest, Principal principal) {
+        return ResponseEntity.ok(chatService.chat(chatRoomId, chatRequest, principal.getName()));
+        }
 
-	@Operation(
-			summary = "ChatRoom Title 수정",
-			description = "ChatRoom Title 수정하는 API")
-	@PutMapping("chatroom/{chatRoomId}")
-	public ResponseEntity<ChatDto.ChatRoomResponse> updateChatRoomTitle(@PathVariable Long chatRoomId, @RequestBody ChatDto.ChatRoomUpdateRequest request){
-		return ResponseEntity.ok(chatService.updateChatRoomTitle(request, chatRoomId));
-	}
+        @Operation(summary = "ChatList 확인", description = "UserId에 따른 CreatedDate 최신순 Chat 목록 조회")
+        @GetMapping("/chat")
+        public ResponseEntity<List<ChatDto.ChatSearchResponse>> chatList (Principal principal){
+            return ResponseEntity.ok(chatService.findByUserIdOrderByCreatedDateDesc(principal.getName()));
+        }
+
+        @Operation(
+                summary = "ChatRoom 생성",
+                description = "ChatRoom 생성하는 API")
+        @PostMapping("/chatroom")
+        public ResponseEntity<ChatDto.ChatRoomResponse> chatRoom (Principal principal){
+            return ResponseEntity.ok(chatService.createChatRoom(principal.getName()));
+        }
+
+        @Operation(
+                summary = "날짜별 ChatRoom 조회",
+                description = "오늘, 어제, 근 일주일, 근 한달 그룹으로 ChatRoom 조회하는 API")
+        @GetMapping("/chatroom")
+        public ResponseEntity<ChatDto.ChatRoomsResponse> getChatRooms (Principal principal){
+            return ResponseEntity.ok(chatService.getChatRooms(principal.getName()));
+        }
+
+        @Operation(
+                summary = "ChatRoom Title 수정",
+                description = "ChatRoom Title 수정하는 API")
+        @PutMapping("chatroom/{chatRoomId}")
+        public ResponseEntity<ChatDto.ChatRoomResponse> updateChatRoomTitle (@PathVariable Long
+        chatRoomId, @RequestBody ChatDto.ChatRoomUpdateRequest request){
+            return ResponseEntity.ok(chatService.updateChatRoomTitle(request, chatRoomId));
+        }
 	/*
 	@Operation(summary = "DB 저장", description = "질문과 답변 DB에 저장")
 	@PostMapping("/save")
@@ -73,4 +73,4 @@ public class ChatController {
 	public FlaskResponse question(@RequestBody FlaskRequest flaskRequest) {
 		return chatService.getPrompt(flaskRequest);
 	}*/
-}
+    }
