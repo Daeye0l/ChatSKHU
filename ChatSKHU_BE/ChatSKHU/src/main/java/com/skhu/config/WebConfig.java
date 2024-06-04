210feaa53e0317312b42dd62ac2514cdecf3145b
Kim, Seokwan <sukwan1106@gmail.com>
2024-06-04 19:33:14 +0900

    마이페이지 조회 시 프로필 이미지도 반환

--- ChatSKHU_BE/ChatSKHU/src/main/java/com/skhu/config/WebConfig.java
+++ ChatSKHU_BE/ChatSKHU/src/main/java/com/skhu/config/WebConfig.java
@@ -1,14 +1,10 @@
 package com.skhu.config;
 
-import com.skhu.common.LoginUserArgumentResolver;
-import com.skhu.common.UserLevelCheckInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,20 +13,6 @@ import java.util.List;
 @RequiredArgsConstructor
 @EnableJpaAuditing
 public class WebConfig implements WebMvcConfigurer {
-
-    private final UserLevelCheckInterceptor userLevelCheckInterceptor;
-
-    private final LoginUserArgumentResolver loginUserArgumentResolver;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(userLevelCheckInterceptor);
-    }
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(loginUserArgumentResolver);
-    }
 
     @Bean
     public RestTemplate restTemplate() {
