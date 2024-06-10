de3f26a79641b7917693e3b081795f15e355d3a8
sinamong0620 <sinaskim60@gmail.com>
2024-06-10 13:51:02 +0900

    chore : 패키지 수정

--- chatskhu-fe/next.config.js
+++ chatskhu-fe/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    images: {
+        domains: ['k.kakaocdn.net'],
+    },
 };
 
 module.exports = nextConfig;
