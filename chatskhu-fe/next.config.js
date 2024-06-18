d41a15147deeebc2d0d77e3656308d6dae729040
sinamong0620 <sinaskim60@gmail.com>
2024-06-18 10:53:11 +0900

    chore : 패키지 매니저 수정

--- chatskhu-fe/next.config.js
+++ chatskhu-fe/next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
     images: {
         domains: ['k.kakaocdn.net'],
     },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
 };
 
 module.exports = nextConfig;
