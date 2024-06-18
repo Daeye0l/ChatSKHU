5090775841c03df4985b2b43d26c4c65b188edac
sinamong0620 <sinaskim60@gmail.com>
2024-06-18 11:12:41 +0900

    #9 docs : 메인 화면 수정

--- chatskhu-fe/pages/main.tsx
+++ chatskhu-fe/pages/main.tsx
@@ -12,14 +12,23 @@ import { useList } from '../store/conversationstore';
 import { useEffect } from 'react';
 import useProfile from '../hooks/useProfile';
 import { userprofile } from '../store/profile';
+import Spinner from '/public/spinner.svg';
+import { useStateStore } from '../store/submitting';
 
 const Main = () => {
     const { isOpen } = useStore();
-    const { responseData, setResponseData } = useList();
+    const { setResponseData } = useList();
 
     const [chatList] = useChatList();
     const [info] = useProfile();
-    const { responseData: userresponse, setResponseData: setUserResponse } = userprofile();
+    const { isSubmitting } = useStateStore();
+    const { setResponseData: setUserResponse } = userprofile();
+
+    const contentArray = [
+        { id: 0, title: '수강신청', subtitle: '수강신청에 대해서 궁금하신가요?' },
+        { id: 1, title: '마이크로전공', subtitle: '마이크로 전공에 대해서 알려드릴까요?' },
+        { id: 2, title: '승연관 위치', subtitle: '자세한 건물 위치가 궁금하세요?' },
+    ];
 
     useEffect(() => {
         if (chatList) {
@@ -52,6 +61,11 @@ const Main = () => {
     };
     return (
         <Layout>
+            {isSubmitting ?? (
+                <SpinnerContainer>
+                    <Spinner />
+                </SpinnerContainer>
+            )}
             <EntireContainer>
                 <Presentation>
                     <CenterContainer>
@@ -60,13 +74,14 @@ const Main = () => {
                             <div>무엇이 궁금한가요?</div>
                         </DefaultImg>
                         <FAQsContainer initial="hidden" animate="visible" variants={container}>
-                            {[0, 1, 2].map((index) => (
-                                <FAQ key={index} variants={item} />
+                            {contentArray.map((index) => (
+                                <FAQ key={index.id} variants={item} title={index.title} subtitle={index.subtitle} />
                             ))}
                         </FAQsContainer>
                     </CenterContainer>
                 </Presentation>
             </EntireContainer>
+
             <Input />
             {isOpen && <Sidebar />}
         </Layout>
@@ -110,4 +125,9 @@ const CenterContainer = styled.main`
     width: 95%;
     display: flex;
     flex-direction: column;
+`;
+const SpinnerContainer = styled.div`
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
 `;
